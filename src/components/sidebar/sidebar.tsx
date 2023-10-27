d30f64faafe1682edc819fbd53c06e3381a8f42b
Vlad Lazar <lazar.vlad151@gmail.com>
2023-10-27 19:34:50 +0200

    setup account, directors, and actors pages and navitgation

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,14 +1,31 @@
 import { Card, List, ListItem } from "@mui/material";
 import { FunctionComponent } from "react";
 import "./style.css";
+import { useNavigate } from "react-router";
 const Sidebar: FunctionComponent = () => {
+  const navigate = useNavigate();
   return (
     <div className="sidebarContainer">
       <Card className="sidebarCard">
         <List>
-          <ListItem className="sidebarItem">Movies</ListItem>
-          <ListItem className="sidebarItem">Actors</ListItem>
-          <ListItem className="sidebarItem">Directors</ListItem>
+          <ListItem
+            className="sidebarItem"
+            onClick={() => navigate("../movies")}
+          >
+            Movies
+          </ListItem>
+          <ListItem
+            className="sidebarItem"
+            onClick={() => navigate("../actors")}
+          >
+            Actors
+          </ListItem>
+          <ListItem
+            className="sidebarItem"
+            onClick={() => navigate("../directors")}
+          >
+            Directors
+          </ListItem>
         </List>
       </Card>
     </div>
