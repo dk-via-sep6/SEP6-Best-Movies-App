d5353de75be45d4f07bc2f5c2db61a0b2da71aac
Vlad Lazar <lazar.vlad151@gmail.com>
2023-10-29 14:02:50 +0100

    added update form to the account page

--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -1,12 +1,45 @@
+import { Card, TextField, Button } from "@mui/material";
 import Sidebar from "../../components/sidebar/sidebar";
 import Topbar from "../../components/topbar/topbar";
-
+import "./style.css";
 const AccountPage: React.FC = () => {
   return (
-    <>
+    <div className="pageContainer">
       <Topbar />
-      <Sidebar />
-    </>
+      <div className="pageContent">
+        <Sidebar />
+        <div className="updateAccountContainer">
+          <Card className="accountCard">
+            <TextField className="textField" label="name" />
+            <TextField type="email" className="textField" label="email" />
+            <TextField type="password" className="textField" label="password" />
+            <TextField
+              type="password"
+              className="textField"
+              label="repeat password"
+            />
+            <Button
+              className="loginButton"
+              variant="contained"
+              onClick={() => {
+                alert("Account updated");
+              }}
+            >
+              Update
+            </Button>
+            <Button
+              className="loginButton"
+              variant="contained"
+              onClick={() => {
+                alert("Account deleted");
+              }}
+            >
+              Delete Account
+            </Button>
+          </Card>
+        </div>
+      </div>
+    </div>
   );
 };
 export default AccountPage;
