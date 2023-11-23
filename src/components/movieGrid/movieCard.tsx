bcadcbd22528a4f62ed8fad3c9566bec1f4c894f
Vlad Lazar <lazar.vlad151@gmail.com>
2023-11-24 00:26:21 +0100

    add card realease date and year

--- src/components/movieGrid/movieCard.tsx
+++ src/components/movieGrid/movieCard.tsx
@@ -5,22 +5,32 @@ import CardActionArea from "@mui/material/CardActionArea";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-
+import "./styles.css";
 interface MovieCardProps {
   title: string;
   imageUrl: string;
+  rating: number;
+  year: number | string;
   // ...other properties like rating, year, etc.
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ title, imageUrl }) => {
+const MovieCard: React.FC<MovieCardProps> = ({
+  title,
+  imageUrl,
+  rating,
+  year,
+}) => {
   return (
     <Card>
-      <CardActionArea>
-        <CardMedia component="img" height="140" image={imageUrl} alt={title} />
+      <CardActionArea className="movieCardArea">
+        <CardMedia component="img" height="200" image={imageUrl} alt={title} />
         <CardContent>
           <Typography gutterBottom variant="h6" component="div">
             {title}
           </Typography>
+
+          <Typography component="div">Rating: {rating}</Typography>
+          <Typography component="div">Release Date:{year}</Typography>
           {/* Additional movie information can go here */}
         </CardContent>
       </CardActionArea>
