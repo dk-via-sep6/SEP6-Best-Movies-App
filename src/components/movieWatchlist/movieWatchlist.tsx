4dadca6ec5464cadc637921e541bf21799dcf65d
Vlad Lazar <lazar.vlad151@gmail.com>
2023-11-28 13:03:44 +0100

    account watchlist ui

---
+++ src/components/movieWatchlist/movieWatchlist.tsx
@@ -0,0 +1,61 @@
+import { Typography } from "@mui/material";
+import { Movie } from "../../model/movie";
+import "./style.css";
+import { DataGrid } from "@mui/x-data-grid";
+import { columns } from "./columns";
+import { useNavigate } from "react-router-dom";
+
+import { useState } from "react";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
+interface MovieWathclistProps {
+  listName: string;
+  movies: Movie[];
+}
+const MovieWatchlist: React.FC<MovieWathclistProps> = ({
+  listName,
+  movies,
+}) => {
+  const rows = movies.map((movie, index) => ({
+    id: index, // DataGrid requires a unique 'id' field for each row
+    title: movie.title,
+    originalLanguage: movie.originalLanguage,
+    releaseDate: movie.releaseDate,
+    genres: movie.genres.join(", "), // Assuming genres is an array
+    voteAverage: movie.voteAverage,
+    voteCount: movie.voteCount,
+  }));
+  const [isGridVisible, setIsGridVisible] = useState(false);
+  const toggleGridVisibility = () => {
+    setIsGridVisible(!isGridVisible);
+  };
+
+  const navigate = useNavigate();
+
+  function handleMovieClick(id: number) {
+    navigate(`/movie/${id}`);
+  }
+
+  return (
+    <div className="watchlist">
+      <div className="watchlistHeader" onClick={toggleGridVisibility}>
+        <Typography variant="h6" className="watchlistTitle">
+          {listName}{" "}
+        </Typography>
+
+        {isGridVisible ? <ArrowDropDownIcon /> : <ArrowRightIcon />}
+      </div>
+      {isGridVisible && (
+        <div className="watchlistContainer">
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            onRowClick={(params) => handleMovieClick(params.row.id)}
+            pageSizeOptions={[10, 20, 30, 100]}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+export default MovieWatchlist;
--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -1,42 +1,75 @@
-import { Card, TextField, Button } from "@mui/material";
-
+import React, { useState } from "react";
+import { Card, TextField, Button, Container } from "@mui/material";
 import "./style.css";
+import { placeholderMovies } from "../movies/placeholderMovies";
+import MovieWatchlist from "../../components/movieWatchlist/movieWatchlist";
+
 const AccountPage: React.FC = () => {
+  const [showAccountCard, setShowAccountCard] = useState(false);
+
+  const handleUpdateClick = () => {
+    setShowAccountCard(true);
+  };
+
+  const handleCancelClick = () => {
+    setShowAccountCard(false);
+  };
+
   return (
-    <div className="pageContainer">
-      <div className="pageLayout">
-        <div className="updateAccountContainer">
-          <Card className="accountCard">
-            <TextField className="textField" label="name" />
-            <TextField type="email" className="textField" label="email" />
-            <TextField type="password" className="textField" label="password" />
-            <TextField
-              type="password"
-              className="textField"
-              label="repeat password"
-            />
-            <Button
-              className="loginButton"
-              variant="contained"
-              onClick={() => {
-                alert("Account updated");
-              }}
-            >
-              Update
-            </Button>
-            <Button
-              className="loginButton"
-              variant="contained"
-              onClick={() => {
-                alert("Account deleted");
-              }}
-            >
-              Delete Account
-            </Button>
-          </Card>
-        </div>
-      </div>
-    </div>
+    <Container className="accountContainer">
+      <MovieWatchlist listName="Watchlist" movies={placeholderMovies} />
+      <MovieWatchlist listName="Halloween movies" movies={placeholderMovies} />
+      <MovieWatchlist listName="Christmas Movies" movies={placeholderMovies} />
+
+      {!showAccountCard && (
+        <Button
+          className="updateButton"
+          variant="contained"
+          onClick={handleUpdateClick}
+        >
+          Update Account
+        </Button>
+      )}
+
+      {showAccountCard && (
+        <Card className="accountCard">
+          <TextField className="textField" label="name" />
+          <TextField type="email" className="textField" label="email" />
+          <TextField type="password" className="textField" label="password" />
+          <TextField
+            type="password"
+            className="textField"
+            label="repeat password"
+          />
+          <Button
+            className="loginButton"
+            variant="contained"
+            onClick={() => {
+              alert("Account updated");
+            }}
+          >
+            Update
+          </Button>
+          <Button
+            className="loginButton"
+            variant="contained"
+            onClick={handleCancelClick}
+          >
+            Cancel
+          </Button>
+          <Button
+            className="loginButton"
+            variant="contained"
+            onClick={() => {
+              alert("Account deleted");
+            }}
+          >
+            Delete Account
+          </Button>
+        </Card>
+      )}
+    </Container>
   );
 };
+
 export default AccountPage;
