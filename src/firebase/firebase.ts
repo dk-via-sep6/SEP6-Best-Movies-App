42fa7bd58717332a333e707c58aa6fba16714fad
Vlad Lazar <lazar.vlad151@gmail.com>
2023-12-10 15:31:23 +0100

    check server url

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,7 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+const serverUrl = process.env.REACT_APP_SERVER_URL;
 
+console.log(serverUrl, "serverUrl");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
